9025a87191119f0064b9402a9f702a589ebc1f18
Xiang Xiao <xxiao23@gmail.com>
2021-07-07 22:37:32 -0700

    add video/audio media recorder and playback

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,23 +5,94 @@ class AudioVideoStream extends React.Component {
   constructor(props) {
     super(props);
     console.log('AudioVideoStream constructor');
+    this.audioCtx = null;
+    this.canvas = null;
+    this.canvasCtx = null;
+    this.mediaRecorder = null;
+    this.requestDataId = null;
+    this.chunks = [];
+    this.mediaBlob = null;
+
+    this.draw = this.draw.bind(this);
+    this.visualize = this.visualize.bind(this);
+    this.startRecord = this.startRecord.bind(this);
+    this.stopRecord = this.stopRecord.bind(this);
+  }
+
+  draw(canvas, canvasCtx, analyser, dataArray, bufferLength) {
+    const WIDTH = canvas.width
+    const HEIGHT = canvas.height;
+
+    requestAnimationFrame(() => {
+      this.draw(canvas, canvasCtx, analyser, dataArray, bufferLength);
+    });
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    let sliceWidth = WIDTH * 1.0 / bufferLength;
+    let x = 0;
+
+
+    for(let i = 0; i < bufferLength; i++) {
+
+      let v = dataArray[i] / 128.0;
+      let y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+
+  }
+
+  visualize(stream) {
+    if (!this.audioCtx) {
+      this.audioCtx = new AudioContext();
+    }
+
+    const source = this.audioCtx.createMediaStreamSource(stream);
+
+    const analyser = this.audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    source.connect(analyser);
+
+    this.draw(this.canvas, this.canvasCtx, analyser, dataArray, bufferLength);
   }
 
   componentDidMount() {
     console.log('componentDidMount');
-    const constraints = {
-      video: { width: { exact: 640 }, height: { exact: 480 } },
-      audio: true,
-    };
+    this.canvas = document.getElementById('visualizer');
+    this.canvasCtx = this.canvas.getContext('2d');
 
     const videoElement = document.querySelector('video');
     if (videoElement) {
       console.log('Find video element');
-      navigator.mediaDevices.getUserMedia(constraints)
+      const vidoeConstraints = {
+        video: { width: { exact: 640 }, height: { exact: 480 } },
+        audio: true,
+      };
+      navigator.mediaDevices.getUserMedia(vidoeConstraints)
         .then((stream) => {
           console.log('stream');
           console.log(stream);
-
 
           console.log('set window stream');
           window.stream = stream;
@@ -30,33 +101,68 @@ class AudioVideoStream extends React.Component {
           console.log(videoElement);
           videoElement.srcObject = stream;
 
-          const track = stream.getVideoTracks()[0];
-          const imageCapture = new ImageCapture(track);
+          console.log('create media recorder');
+          this.mediaRecorder = new MediaRecorder(stream);
 
-          setInterval(() => {
-            imageCapture.takePhoto()
-              .then(blob => {
-                console.log('take snapshot')
-                console.log(blob);
-              })
-              .catch(error => console.log(error));
-          }, 5000);
+          this.visualize(stream);
+
+          this.mediaRecorder.ondataavailable = (event) => {
+            console.log('media recorder data available');
+            if (event.data.size > 0) {
+              console.log('media recorder data:');
+              console.log(event.data);
+              this.chunks.push(event.data);
+              console.log(this.chunks);
+            } else {
+              console.log('empty media recorder data');
+            }
+          }
+          console.log(this.mediaRecorder.state);
+
+          this.mediaRecorder.onstop = (event) => {
+            this.mediaBlob = new Blob(this.chunks, {'type' : 'video/webm'});
+            this.chunks = [];
+            const player = document.getElementById('playback');
+            player.src = window.URL.createObjectURL(this.mediaBlob);
+            console.log('load playback');
+          }
         })
         .catch(error => console.error(error));
     } else {
-      console.error('Can\'t find video elemeent');
+      console.error('Can\'t find video element');
     }
+  }
 
+  startRecord() {
+    this.mediaRecorder.start();
+    this.requestDataId = setInterval(() => {
+      this.mediaRecorder.requestData();
+    }, 5000);
+    console.log(this.mediaRecorder.state);
+    console.log('recorder started');
+  }
+
+  stopRecord() {
+    clearInterval(this.requestDataId);
+    this.mediaRecorder.stop();
+    console.log(this.mediaRecorder.state);
+    console.log('recorder stopped');
   }
 
   render() {
     return (
-      <div>
+      <section>
         <video 
           autoPlay 
           muted
           playsInline />
-      </div>
+        <canvas id='visualizer' height='60px' />
+        <div id="buttons">
+          <button id='record' onClick={this.startRecord}>Record</button>
+          <button id='stop' onClick={this.stopRecord}>Stop</button>
+        </div>
+        <video id='playback' controls />
+      </section>
     );
   }
 }
